eebf9408cc577cbf6556a4a62b8d32a28611164a
Eva <mylt432@gmail.com>
2025-03-25 20:00:01 +0300

    create sass

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-
-
+import { defineConfig } from 'vite';
 export default defineConfig({
- server: {
+  server: {
     port: 3000,
     open: true,
- },
- build: {
-      outDir: "../build"
- }
-})
+  },
+  build: {
+    outDir: '../build',
+    emptyOutDir: true,
+  },
+});
