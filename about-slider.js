83038e5ef2cea038e71c259740020ab1b7e77f04
Eva <mylt432@gmail.com>
2025-09-11 21:03:26 +0300

    a new branch created and hero section on the new page about

---
+++ about-slider.js
@@ -0,0 +1,22 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+export const useAboutSlider = () => {
+  new Swiper('.hero__slider', {
+    spaceBetween: 32,
+    slidesPerView: 1,
+    breakpoints: {
+      576: {
+        slidesPerView: 1.5,
+      },
+      768: {
+        slidesPerView: 2.5,
+      },
+    },
+    loop: true,
+    modules: [Navigation],
+    navigation: {
+      nextEl: '.hero__slider-btn',
+    },
+  });
+};
---
+++ about.js
@@ -0,0 +1,9 @@
+import './scss/about.scss';
+
+// components
+import { useTheme } from './theme';
+useTheme();
+import { useBurger } from './burger';
+useBurger();
+import { useAboutSlider } from './about-slider';
+useAboutSlider();
